5cadc4b7c502513c9248c36907a99879ea394e7d
diseemm <trankhanhduong112@gmail.com>
2020-05-28 10:35:06 +0700

    Labwork 8th

---
+++ 08.practical.work.client.nonblock.c
@@ -0,0 +1,61 @@
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <fcntl.h>
+#define PORT 8784
+
+extern ssize_t send(), recv();
+
+int main() {
+    char buffer[128];
+    char hostname[128];
+    printf("Enter: ");
+    scanf("%s", hostname);
+
+    struct sockaddr_in servaddr;
+    struct hostent *hostip = gethostbyname(hostname);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if (sockfd < 0) {
+        printf("Error creating socket!\n");
+        exit(0);
+    } else printf("Created!\n");
+
+    if (hostip == NULL) {
+        printf("Unknown host!\n");
+        exit(0);
+    } else printf("Got host!\n");
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    memcpy((char *) &servaddr.sin_addr.s_addr, hostip->h_addr_list[0], hostip->h_length);
+    servaddr.sin_port = htons(PORT);
+
+    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        printf("Can't connect!\n");
+        exit(0);
+    } else {
+        printf("Connected!\n");
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+        fcntl(sockfd, F_SETFL, O_NONBLOCK);
+
+        while (1) {
+            printf("Enter: ");
+            memset(buffer, 0, sizeof(buffer));
+            scanf("%s", buffer);
+            if (strcmp(buffer, "/quit") == 0)   break;
+            send(sockfd, buffer, strlen(buffer)+1, 0);
+            memset(buffer, sizeof(buffer), 0);
+            if (recv(sockfd, buffer, sizeof(buffer), 0) == 0) {
+                printf("Error!\n");
+                exit(0);
+            } else {
+                    printf("From server: %s\n", buffer);
+                    exit(0);
+                }
+            }
+        }
+    return 0;
+}
---
+++ 08.practical.work.server.nonblock.c
@@ -0,0 +1,78 @@
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <fcntl.h>
+#include <unistd.h>
+#define PORT 8784
+
+extern ssize_t send(), recv();
+
+int main() {
+    char buffer[128];
+    int sockfd, clen, clientfd;
+    struct sockaddr_in saddr, caddr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        printf("Error creating socket.\n");
+        exit(0);
+    } else printf("Successfully created!\n");
+    
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(PORT);
+
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+    int fl = fcntl(sockfd, F_GETFL, 0);
+
+    if (bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) != 0) {
+        printf("Error binding.\n");
+        exit(0);
+    } else printf("Successfully bind!\n");
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    if (listen(sockfd, 5) < 0) {
+        printf("Error listening.\n");
+        exit(0);
+    } else printf("Listening\n");
+
+    clen = sizeof(caddr);
+    
+    while(1) {
+        clientfd = accept(sockfd, (struct sockaddr *) &caddr, (unsigned int *)&clen);
+        if (clientfd < 0) {
+            printf("Error accepting connection.\n");
+            exit(0);
+        } else {    
+            printf("Accepted!\n");
+            fcntl(sockfd, F_SETFL, O_NONBLOCK);
+            while (1) {
+                int n = recv(clientfd, buffer, sizeof(buffer), 0);
+                if (n == 0) {
+                    printf("Error!!!\n");
+                    exit(0);
+                } else {
+                    int firstChar = 0;
+                    for (int i = 0; i < n; i++) {
+                        if (buffer[i] == '\0') {
+                            printf("From client: %s\n", &buffer[firstChar]);
+                            firstChar = i+1;
+                        }
+                    }
+                    memset(buffer, 0, sizeof(buffer));
+                    printf("Enter: ");
+                    scanf("%s", buffer);
+                    if (strcmp(buffer, "/dc") == 0)    break;
+                    send(clientfd, buffer, strlen(buffer)+1, 0);
+                }
+            }
+        }
+    }
+
+    close(clientfd);
+    return 0;
+}
